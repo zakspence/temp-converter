24c97fa5d54153abc9a9ca4ab3ff8f6ff5d2027e
Zak Spence <zakspence@gmail.com>
2017-01-23 21:24:33 -0600

    temp converter complete, buttons are wired up, color change and output function at 80%

--- converter.js
+++ converter.js
@@ -1,8 +1,25 @@
+/*---------------------------------
+
+		  	 THE STAGE
+
+----------------------------------*/
+
+
 var tempField = document.getElementById('temp-field');
 var submitButton = document.getElementById('submit');
+var resetButton = document.getElementById('reset');
+var display = document.getElementById('display');
 
 tempField.addEventListener('keyup', check13);
-submitButton.addEventListener('click', convert);
+submitButton.addEventListener('click', function(){colorChange(convert());});
+resetButton.addEventListener('click', reset);
+
+
+/*---------------------------------
+
+		  FORM FUNCTIONALITY
+
+----------------------------------*/
 
 
 function check13(event) {
@@ -11,11 +28,93 @@ function check13(event) {
 	}
 }
 
-function convert(event) {
-	var userTemp = event.target.value;
+function checkRadio() {
+	var isFahrenheit = document.getElementById('fahrenheit').checked;
+
+	return isFahrenheit;
+}
+
+function reset() {
+	tempField.setAttribute('value', 0);
+	display.innerHTML = '';
+}
+
+
+
+/*---------------------------------
+
+		TEMPERATURE CONVERSION 
+
+----------------------------------*/
 	
 
+function convert(event) {
+	var userTemp = event.target.value;
+	var isFahrenheit = checkRadio();
 
+	if (isFahrenheit) {
+
+	//FAHRENHEIT 2 CELSIUS
+	//Deduct 32, then multiply by 5, then divide by 9
+	return celsius = {
+		temp: (userTemp -32) * 5 / 9,
+		scale: 'celsius'
+	};
+
+	} else {
+
+	//CELSIUS 2 FAHRENHEIT
+	//Multiply by 9, then divide by 5, then add 32
+	return fahrenheit = {
+		temp: (userTemp * 9 / 5) + 32,
+		scale: 'fahrenheit'
+	};
 }
+
+
+/*---------------------------------
+
+			COLOR CHANGE 
+
+----------------------------------*/
+
+function colorChange(temp) {
+	display.innerHTML = (function () {
+		//  When the temperature is shown in celsius, color text by heat
+		if (temp.scale === 'celsius' ){
+			if (temp.temp < 0 ){
+				return  // blue text
+
+			} else if (temp.temp > 32 ){
+				return // red text
+
+			} else {
+				return // green text
+
+			}
+		}
+		//  When the temperature is shown in fahrenheit, color text by heat
+		else if (temp.scale === 'fahrenheit' ){
+			if (temp.temp < 32 ){
+				return  // blue text
+
+			} else if (temp.temp > 90 ){
+				return // red text
+
+			} else {
+				return // green text
+
+			}
+		}
+	}());
+}
+
+
+
+
+
+
+
+
 
 
